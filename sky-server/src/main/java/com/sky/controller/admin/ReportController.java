a72bba63315f900297e4c28b3ec53093edfe1b7c
litaibai <liyunian23@gmail.com>
2025-08-04 17:59:46 +0800

    导出excel表格，项目完结

--- sky-server/src/main/java/com/sky/controller/admin/ReportController.java
+++ sky-server/src/main/java/com/sky/controller/admin/ReportController.java
@@ -15,6 +15,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletResponse;
 import java.time.LocalDate;
 
 /**
@@ -63,5 +64,12 @@ public class ReportController {
                        @DateTimeFormat (pattern = "yyyy-MM-dd") LocalDate end){
         log.info("销量排名：{}到{}", begin, end);
         return Result.success(reportService.top10(begin, end));
+    }
+
+    @GetMapping("/export")
+    @ApiOperation("导出数据")
+    public void export(HttpServletResponse response){
+        reportService.exportBusinessData(response);
+        log.info("导出数据");
     }
 }
